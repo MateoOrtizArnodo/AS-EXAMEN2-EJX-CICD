08953ff1fad70d3f3a1180559693ff32c143ec50
matutecott <mateortizarnodo@gmail.com>
2025-12-16 16:46:25 +0100

    prueba2

--- code/test.py
+++ code/test.py
@@ -17,5 +17,5 @@ def test_setbalance():
 
 def test_setbalance():
     obj = Wallet(0)
-    obj.set_balance(10)
-    assert obj.get_balance() == 10
+    obj.set_balance(100)
+    assert obj.get_balance() == 100 
